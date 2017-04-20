5b5478a2f9b1c5e9f6a9efeb9b936ed0de488141
capricornus <aleksandrzhupanov@gmail.com>
2017-04-20 20:59:27 +0300

    persistentOkButSlowly

---
+++ src/main/java/com/jubaka/sors/appserver/dao/PayloadDao.java
@@ -0,0 +1,7 @@
+package com.jubaka.sors.appserver.dao;
+
+/**
+ * Created by root on 18.04.17.
+ */
+public class PayloadDao {
+}
---
+++ src/main/java/com/jubaka/sors/appserver/service/BeanEntityConverter.java
@@ -0,0 +1,7 @@
+package com.jubaka.sors.appserver.service;
+
+/**
+ * Created by root on 19.04.17.
+ */
+public class BeanEntityConverter {
+}
